83287c9d824ff7b94fc91e15269d8732bc570ed5
mywnajsldkf <mywnajsldkf@gmail.com>
2023-05-29 14:34:39 +0000

    🔨 fix : wakeup_ticks 타입 수정 #4
    - uint64_t -> int64_t

--- include/threads/thread.h
+++ include/threads/thread.h
@@ -91,7 +91,7 @@ struct thread {
 	enum thread_status status;          /* Thread state. */
 	char name[16];                      /* Name (for debugging purposes). */
 	int priority;                       /* Priority. */
-	uint64_t wakeup_ticks;
+	int64_t wakeup_ticks;
 
 	/* Shared between thread.c and synch.c. */
 	struct list_elem elem;              /* List element. */
