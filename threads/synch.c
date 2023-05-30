c95d5f98c9515c6b82c11571d30e3bf0bee3266f
mywnajsldkf <mywnajsldkf@gmail.com>
2023-05-30 23:17:59 +0000

    ✨ feat : sema_down #10
    - 세마포어를 획득하고 waiters list에 우선순위로 삽입한다.

--- threads/synch.c
+++ threads/synch.c
@@ -66,7 +66,8 @@ sema_down (struct semaphore *sema) {
 
 	old_level = intr_disable ();
 	while (sema->value == 0) {
-		list_push_back (&sema->waiters, &thread_current ()->elem);
+		// list_push_back (&sema->waiters, &thread_current ()->elem);
+		list_insert_ordered (&sema->waiters, &thread_current()->elem, cmp_priority, NULL);
 		thread_block ();
 	}
 	sema->value--;
