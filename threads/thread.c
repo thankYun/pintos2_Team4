cca6dc34a5c2c6394ab4bf0a6759b5a4d8b8dfa0
mywnajsldkf <mywnajsldkf@gmail.com>
2023-05-30 12:10:49 +0000

    ✨ feat : 우선순위 정렬 기능 추가 #10
    - 스레드가 unblock될 때, 스레드가 우선순위로 정렬된다.

--- threads/thread.c
+++ threads/thread.c
@@ -302,7 +302,7 @@ thread_unblock (struct thread *t) {
 
 	old_level = intr_disable ();
 	ASSERT (t->status == THREAD_BLOCKED);
-	list_push_back (&ready_list, &t->elem);
+	list_insert_ordered(&ready_list, &t->elem, cmp_priority, NULL);
 	t->status = THREAD_READY;
 	intr_set_level (old_level);
 }
